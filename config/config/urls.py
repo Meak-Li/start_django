3974d76b8c057101efaaa2450ccd9b59409d2a15
Meak-li <meak.li@silksoftware.com>
2023-03-18 15:10:04 +0800

    引入其他子应用的url文件

--- config/config/urls.py
+++ config/config/urls.py
@@ -22,8 +22,16 @@ from user.views import login
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login', login),
-    path('', include(urls))
+    path('', include(urls)),  # 增加其他的url路由地址
 ]
 #  自定义错误页面, 这个自定义的错误页面必须在主url.py页面里面加入
 #  还需要注意一点，需要将debug模式关闭，允许访问的地址更改为*
 handler404 = 'user.views.page_not_found'  # 后面是对应404错误的地址
+
+
+"""
+几种方式添加:
+可以直接写在上面：extra_url.
+在对应的子应用里面直接添加url文件，使用include函数可以使用。
+include(), 当很多的url都有相同的前缀时，可以使用include[]
+"""
