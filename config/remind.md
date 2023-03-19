28c55330150d849bcdc22e3060ca1c0429d1d9ce
Meak-li <meak.li@silksoftware.com>
2023-03-19 15:25:01 +0800

    多对多关系模型建立

--- config/remind.md
+++ config/remind.md
@@ -1,2 +1,15 @@
 ### 可以直接生成一个requirement的安装包文件
+~~~
 pip freeze > requirements.txt
+~~~
+
+## Model
+### 逆向生成一个表, 先有表，然后需要生成对应的model
+~~~ 
+python manage.py inspectdb > model_app/models.py
+~~~
+### 字段
+~~~
+BooleanField, CharField, ....官网上面有
+UUIDField(primary_key=True, defult=uuid.uuid4) 唯一
+~~~
--- config/user_app/models.py
+++ config/user_app/models.py
@@ -3,4 +3,64 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    pass
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    age = models.CharField(max_length=32)
+
+    class Meta:
+        db_table = 't_user'  # 表数据库里面名称
+
+
+class PLace(models.Model):
+    name = models.CharField(max_length=32)
+    address = models.CharField(max_length=64)
+
+    class Meta:
+        db_table = 't_place'
+
+
+class Restaurant(models.Model):
+    palace = models.OneToOneField(PLace, on_delete=models.CASCADE, primary_key=True)
+    servers_hot = models.BooleanField(default=False)
+    servers_cool = models.BooleanField(default=False)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+
+    class Meta:
+        db_table = 't_restaurant'
+
+
+class SchoolClass(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=32)
+    school_class = models.ManyToManyField(SchoolClass)  # 会自动生成第三方表， 自定义的时候需要新建一个第三方表，并且里面有两个外键，还需要建立一个多对多的字段
+
+
+# 自定义第三方的表，多对多表设计
+class Person(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=32)
+    person_language = models.ManyToManyField(
+        Person,
+        through='PersonLanguage',
+        through_fields=('language', 'person')  # 当第三方表里面存在多个外键的时候，需要指定，多对多需要关联的是哪两个表，且其他的表需要指定related_name字段
+    )
+
+
+class PersonLanguage(models.Model):
+    LEVEL = (
+        (1, 'A'),
+        (2, 'B'),
+    )
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Person, on_delete=models.CASCADE,
+                                   related_name="restaurant_person")  # related_name反向引用的名字
+    level = models.CharField(choices=LEVEL, max_length=32)  # 可选字段的写法
